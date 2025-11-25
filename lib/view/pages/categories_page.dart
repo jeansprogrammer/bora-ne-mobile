134f54b2ebe387b064c180b0de0b5c4b7878f99c
jeansprogrammer <jeansilva20042019@gmail.com>
2025-11-24 21:45:56 -0300

    criação de lista de categorias

---
+++ lib/view/pages/categories_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:boranemobile/models/category_model.dart';
+import 'package:boranemobile/view/widgets/category_item.dart';
+
+class CategoriesPage extends StatelessWidget {
+  CategoriesPage({super.key});
+
+  final List<CategoryModel> categories = [
+    CategoryModel(title: "Religioso", image: "../assets/images/bible.png"),
+    CategoryModel(title: "Gastronômico", image: "../assets/images/burger.png"),
+    CategoryModel(title: "Histórico", image: "../assets/images/hieroglyph.png"),
+    CategoryModel(title: "Aventuras", image: "../assets/images/tent.png"),
+    CategoryModel(title: "Outros", image: "../assets/images/searching.png"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(255, 253, 245, 1),
+
+      appBar: AppBar(
+        title: const Text(
+          "Categorias",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView.builder(
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            return CategoryItem(category: categories[index]);
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/widgets/category_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:boranemobile/models/category_model.dart';
+
+class CategoryItem extends StatelessWidget {
+  final CategoryModel category;
+
+  const CategoryItem({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(22),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          // Imagem
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(
+              category.image,
+              height: 90,
+              fit: BoxFit.fill,
+            ),
+          ),
+
+          const SizedBox(width: 16),
+
+          // Título
+          Expanded(
+            child: Text(
+              category.title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
