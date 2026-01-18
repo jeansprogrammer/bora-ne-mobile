b10efdcabd16f5a17e5c964c7dde303a4a853360
Antonio0203 <carvalhos.tor07@gmail.com>
2026-01-18 16:55:13 -0300

    Corrigi o código do main.dart e login1_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'controllers/route_creation_controller.dart';
 import 'view/pages/splash.dart';
+import 'screens/login1_screen.dart';
+import 'screens/login2_screen.dart';
 
 void main() {
   runApp(
@@ -30,29 +32,9 @@ class BoraNE extends StatelessWidget {
         fontFamily: 'Poppins',
       ),
       home: const SplashScreen(),
-    );
-  }
-}
-
-class BoraNE extends StatelessWidget {
-  const BoraNE({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bora NE',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Poppins',
-      ),
-
-      home: const SplashScreen(),
-
       routes: {
-        '/login1': (context) => const Login1Screen(),
-        '/login2': (context) => const Login2Screen(),
+        '/login1': (context) => Login1Screen(),
+        '/login2': (context) => Login2Screen(),
       },
     );
   }
