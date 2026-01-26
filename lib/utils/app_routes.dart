33752b9c337f5ba84a7bd3e328069e61e7bf27e5
josevieirac <josevieira1709@gmail.com>
2026-01-26 19:51:19 -0300

    Revert "Adicionei a estrtutura de código nos arquivos da pasta utils"
    This reverts commit c39fd25f9d2a3cd3b63055f61e16a4b3d4e5dcd2.

--- lib/utils/app_routes.dart
+++
@@ -1,8 +0,0 @@
-/// Rotas do aplicativo
-class AppRoutes {
-  AppRoutes._();
-
-  static const String login1 = '/login1';
-  static const String login2 = '/login2';
-  static const String home = '/home';
-}
