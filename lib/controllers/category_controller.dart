9c973681270f5a0881779367697ac75614ca92d2
Antonio0203 <carvalhos.tor07@gmail.com>
2026-05-27 21:59:19 -0300

    Acrescentei a conexão ao FireBase, mas apenas da categoria "Religiosos"

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -1,7 +1,18 @@
 import 'package:boranemobile/models/category_model.dart';
+import 'package:boranemobile/models/destino_model.dart'; //Aqui estou importando o modelo já existente
+import 'package:cloud_firestore/cloud_firestore.dart'; //Aqui estou importando o Firebase
 import 'package:flutter/material.dart';
 
 class CategoryController extends ChangeNotifier {
+
+  // Aqui será a instância do Firebase Firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Segue à baixo as Novas variáveis de Estado
+
+  List<DestinoModel> destinosFiltrados = [];
+bool isLoading = false;
+
   final List<CategoryModel> categories = [
     CategoryModel(
       title: "Religioso",
@@ -29,6 +40,36 @@ class CategoryController extends ChangeNotifier {
       route: "/outros",
     ),
   ];
+
+  // Método para puxar do Firebase
+
+  Future<void> carregarDestinosPorCategoria(String nomeCategoria) async {
+    isLoading = true;
+    destinosFiltrados = [];
+    notifyListeners(); // Isso vai servir para avisar a View para mostrar o indicador de carregamento
+
+    try {
+      // Segue à baixo o responsável por fazer a busca na coleção "destinos", onde o array "categorias" contém o nome enviado
+      QuerySnapshot querySnapshot = await _firestore
+        .collection('destinos')
+        .where('categorias', arrayContains: nomeCategoria)
+        .get();
+
+      // Agora segue à baixo o responsável por mapeiar os documentos usando o DestinoModel.fromMap que já possuímos
+      destinosFiltrados = querySnapshot.docs.map((doc) {
+        return DestinoModel.fromMap(
+          doc.data() as Map<String, dynamic>,
+          id: doc.id,
+        );
+      }).toList();
+    } catch (e) {
+      print("Erro ao buscar destinos por categorias: $e");
+    } finally {
+      isLoading = false;
+      notifyListeners(); // Este vai avisar a View para renderizar os destinos ou parar loading
+    }
+  }
+
 
   void navigateToCategory(BuildContext context, String route) {
     Navigator.pushNamed(context, route);
